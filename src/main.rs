1e99aa91b6969ed53b237ca39c9ecb2aa3103ee7
noah figueras <noahfigueras@gmail.com>
2024-04-14 21:58:34 +0200

    refactor code + gas auto

--- src/main.rs
+++ src/main.rs
@@ -1,83 +1,42 @@
-use std::{ env, path::Path };
-use c_kzg::{
-    KzgCommitment, 
-    KzgProof, 
-    KzgSettings,
-    Blob
-};
 use alloy::{
-    network::{ Ethereum, TxSigner },
+    network::EthereumSigner,
     providers::{ Provider, ProviderBuilder },
-    rpc::client::RpcClient,
-    signers::{
-        wallet::LocalWallet,
-    },
-    primitives::{ bytes, U256, FixedBytes},
+    rpc::types::eth::TransactionRequest,
+    signers::wallet::LocalWallet,
+    primitives::Address,
     consensus::{
-        TxEip4844,
-        TxEip4844Variant,
-        BlobTransactionSidecar,
-        SignableTransaction,
-        TxEnvelope,
+        SidecarBuilder,
+        SimpleCoder,
     },
-    eips::{
-        eip2930::AccessList,
-        eip4844::BYTES_PER_BLOB,
-        eip2718::Encodable2718
-    }
 };
 
 use eyre::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let wallet: LocalWallet = env!("PK").parse()?;
+    let wallet: LocalWallet = env!("PK").parse().unwrap();
     let addr = wallet.address();
-    let client = RpcClient::new_http(env!("RPC_URL").parse()?);
-    let provider = ProviderBuilder::<_,Ethereum>::new()
-                    .on_client(client);
+    let signer = EthereumSigner::from(wallet);
+    let provider = ProviderBuilder::new()
+        .with_recommended_fillers()
+        .signer(signer)
+        .on_http(env!("RPC_URL").parse().unwrap()).unwrap();
 
-    let nonce = provider.get_transaction_count(addr, None).await?;
-    let estimation = provider.estimate_eip1559_fees(None).await?;
+    let blob_data = "---- Hello Blobs ----".as_bytes();
+    let sidecar = SidecarBuilder::<SimpleCoder>::from_slice(&blob_data)
+        .build().unwrap();
 
-    let trusted_setup = KzgSettings::load_trusted_setup_file(Path::new("./trusted_setup.txt"))?;
-    let blob_data = [41; BYTES_PER_BLOB];
-    let blob = Blob::new(blob_data);
-    let commitment = KzgCommitment::blob_to_kzg_commitment(&blob, &trusted_setup)?;
-    let proof = KzgProof::compute_blob_kzg_proof(&blob, &commitment.to_bytes(), &trusted_setup)?;
-
-    let sidecar = BlobTransactionSidecar::new(
-        vec![FixedBytes::new(blob_data)],
-        vec![FixedBytes::new(commitment.to_bytes().into_inner())],
-        vec![FixedBytes::new(proof.to_bytes().into_inner())]
-    );
-
-    // TODO: Gas Estimation Optimization
-    let fee = provider.get_gas_price().await?;
-    let max_fee = provider.get_max_priority_fee_per_gas().await?;
-
-    let tx = TxEip4844 {
-        chain_id: 17000, // Holesky
-        nonce: nonce.to_string().parse().unwrap(),
-        gas_limit: 21_000,
-        max_fee_per_gas: 600_000_000_000,//fee.to_string().parse()?, // max (baseFee + priorityFee) refunds rest
-        max_priority_fee_per_gas: 6_000_000_000,//max_fee.to_string().parse()?, 
-        to: addr, 
-        value: U256::from(0),
-        access_list: AccessList(vec![]),
-        blob_versioned_hashes: sidecar.versioned_hashes().collect(),
-        max_fee_per_blob_gas: 30_000_000_000,
-        input: bytes!(),
+    let tx = TransactionRequest {
+        from: Some(addr),
+        to: Some(Address::ZERO),
+        blob_versioned_hashes: Some(sidecar.versioned_hashes().collect()),
+        sidecar: Some(sidecar),
+        ..Default::default()
     };
 
-    // Sign and submit 
-    let mut variant = TxEip4844Variant::from((tx, sidecar));
-    let signature = wallet.sign_transaction(&mut variant).await?;
-    let tx_signed = variant.into_signed(signature);
-    let tx_envelope: TxEnvelope = tx_signed.into();
-    let encoded = tx_envelope.encoded_2718();
+    let tx = provider.send_transaction(tx).await.unwrap();
+    let receipt = tx.get_receipt().await.unwrap();
+    println!("{:?}", receipt);
 
-    let result = provider.send_raw_transaction(&encoded).await?;
-    println!("{:?}", result);
     Ok(())
 }
